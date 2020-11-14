b19f9cbcf641c91b60e891cd2a8f7173b3f031fb
ashrafuddin1496 <asraf1496@gmail.com>
2020-11-14 09:36:33 +0600

    composer and package setup done

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '41f84578419665d8af5cb37ac7fdc4cc8edd0058',
+    'name' => 'ashrafuddin1496/edu-board',
+  ),
+  'versions' => 
+  array (
+    'ashrafuddin1496/edu-board' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '41f84578419665d8af5cb37ac7fdc4cc8edd0058',
+    ),
+  ),
+);
